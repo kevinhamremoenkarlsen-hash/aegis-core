5210a237e5494fa786dccd0c670822856940ce6f
kevinhamremoenkarlsen-hash <kevinhamremoenkarlsen@gmail.com>
2026-09-04 09:02:08 +0200

    Add initial kernel entry point and panic handler

---
+++ kernel/src/main.rs
@@ -0,0 +1,23 @@
+```rust
+#![no_std]
+#![no_main]
+#![deny(unsafe_op_in_unsafe_fn)]
+
+use bootloader_api::{entry_point, BootInfo};
+use core::panic::PanicInfo;
+
+entry_point!(kernel_main);
+
+fn kernel_main(_boot_info: &'static mut BootInfo) -> ! {
+    loop {
+        core::hint::spin_loop();
+    }
+}
+
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    loop {
+        core::hint::spin_loop();
+    }
+}
+```
